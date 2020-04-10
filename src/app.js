159e89b55fd7ede803855f49698ba63c8384b7f7
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-10 20:40:07 -0300

    method POST and GET

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid} = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,14 +12,14 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const {title, url, techs} = request.body;
-  console.log(request.body);
-
-  return response.json({message: 'retorno temporario'});
+  const project = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(project);
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
