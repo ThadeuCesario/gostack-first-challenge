421d7c892b56347ddb798dc2ab9d79ffb1507dce
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-10 21:40:54 -0300

    method PUT

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const {uuid} = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
-
 const app = express();
 
 app.use(express.json());
@@ -12,25 +10,31 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  return response.json(repositories);
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const {title, url, techs} = request.body;
   const project = {id: uuid(), title, url, techs, likes: 0};
   repositories.push(project);
-  return response.json(project);
+  return response.status(200).json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const {id} = request.params;
+  const {title, url, techs} = request.body;
+  const repositoryIndex = repositories.findIndex(repositorie => repositorie.id === id);
 
-  if(repositories.findIndex(repositorie => repositorie.id === id)){
-    repositories.splice(repositorie, 1);
-    return response.json({message: 'its the same ID'})
+  if(repositoryIndex < 0){
+    return response.status(404).json({message: 'Repository not found'});
   }
+  
+  repositories[repositoryIndex].title = title;
+  repositories[repositoryIndex].url = url;
+  repositories[repositoryIndex].techs = techs;
 
-  return response.json({message: 'Element not found'});
+  return response.status(200).json(repositories[repositoryIndex]);
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
